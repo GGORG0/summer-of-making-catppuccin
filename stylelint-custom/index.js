b458d37cac46eea94db63b5babc1f3d794480981
GGORG <GGORG0@protonmail.com>
2025-07-22 00:09:11 +0200

    initial setup

---
+++ stylelint-custom/index.js
@@ -0,0 +1,5 @@
+import colorOperation from "./color-operation.js";
+import optimizedSvgs from "./optimized-svgs.js";
+import redundantParentSelector from "./redundant-parent-selector.js";
+
+export default [colorOperation, optimizedSvgs, redundantParentSelector];
